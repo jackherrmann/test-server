b161cac2059ea6ed0811d022f8b801bb1f735537
Jack Herrmann <jackherrmann111@gmail.com>
2020-09-11 22:57:27 -0400

    small changes

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express'); 
 const app = express(); 
 
-let port = process.env.port || 3000; 
+const port = process.env.port || 3000; 
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`); 
